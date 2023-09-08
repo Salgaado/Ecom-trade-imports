d559fdfcc167c420953b6926852fc7302414f7a4
Daniel Salgado <dansalgaado@gmail.com>
2023-09-08 03:38:29 -0300

    debug test

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const menu = document.getElementById("menu");
-const flashComponent = document.getElementById("flash-component");
+// const menu = document.getElementById("menu");
+// const flashComponent = document.getElementById("flash-component");
 
-window.addEventListener("scroll", () => {
-  // Obtém a altura do componente flash
-  const flashComponentHeight = flashComponent.offsetHeight;
-  // Obtém a posição atual do scroll vertical
-  const scrollY = window.scrollY;
+// window.addEventListener("scroll", () => {
+//   // Obtém a altura do componente flash
+//   const flashComponentHeight = flashComponent.offsetHeight;
+//   // Obtém a posição atual do scroll vertical
+//   const scrollY = window.scrollY;
   
-  if (scrollY >= flashComponentHeight) {
-    // Quando o scroll atinge a altura do componente flash
-    menu.classList.add("fixed");
-  } else {
-    // Quando o scroll está acima da altura do componente flash
-    menu.classList.remove("fixed");
-  }
-});
+//   if (scrollY >= flashComponentHeight) {
+//     // Quando o scroll atinge a altura do componente flash
+//     menu.classList.add("fixed");
+//   } else {
+//     // Quando o scroll está acima da altura do componente flash
+//     menu.classList.remove("fixed");
+//   }
+// });
 
 
 document.addEventListener("DOMContentLoaded", function() {
