e02a99af0da7e6d1fe532492aef047c14bba488d
Daniel Salgado <dansalgaado@gmail.com>
2023-09-08 03:33:11 -0300

    debug test

--- main.js
+++ main.js
@@ -1,35 +1,20 @@
 const menu = document.getElementById("menu");
 const flashComponent = document.getElementById("flash-component");
 
-function handleScroll() {
-  // Verifica a largura da janela
-  if (window.innerWidth > 640) {
-    // Obtém a altura do componente flash
-    const flashComponentHeight = flashComponent.offsetHeight;
-    // Obtém a posição atual do scroll vertical
-    const scrollY = window.scrollY;
-
-    if (scrollY >= flashComponentHeight) {
-      // Quando o scroll atinge a altura do componente flash
-      menu.classList.add("fixed");
-    } else {
-      // Quando o scroll está acima da altura do componente flash
-      menu.classList.remove("fixed");
-    }
+window.addEventListener("scroll", () => {
+  // Obtém a altura do componente flash
+  const flashComponentHeight = flashComponent.offsetHeight;
+  // Obtém a posição atual do scroll vertical
+  const scrollY = window.scrollY;
+  
+  if (scrollY >= flashComponentHeight) {
+    // Quando o scroll atinge a altura do componente flash
+    menu.classList.add("fixed");
   } else {
-    // Se a largura da janela for menor ou igual a 640px, remova a classe "fixed" do menu
+    // Quando o scroll está acima da altura do componente flash
     menu.classList.remove("fixed");
   }
-}
-
-// Adicione um ouvinte de evento de scroll à janela
-window.addEventListener("scroll", handleScroll);
-
-// Adicione um ouvinte de evento de redimensionamento da janela
-window.addEventListener("resize", handleScroll);
-
-// Chame a função handleScroll para verificar o estado inicial da página
-handleScroll();
+});
 
 
 document.addEventListener("DOMContentLoaded", function() {
